c251dfac15820c8c0ff9c62b9eeba0f20b07f5b8
mayanhavoc <mayanhavoc@gmail.com>
2021-08-15 12:47:48 -0700

    style on cluster map

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/mayanhavoc/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
